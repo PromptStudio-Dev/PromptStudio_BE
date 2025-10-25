90a3cce7aab9b5cca774d68a34d5b8c8b09b30a4
dev-cyan <zyos1@naver.com>
2025-10-25 21:26:22 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/promptstudio/promptstudio/PromptstudioApplication.java
@@ -0,0 +1,13 @@
+package promptstudio.promptstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptstudioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromptstudioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/promptstudio/promptstudio/PromptstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package promptstudio.promptstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromptstudioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
