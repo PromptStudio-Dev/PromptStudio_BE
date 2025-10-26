2fb888e11eb5312ac09d10152ed205f81516e0ee
dev-cyan <zyos1@naver.com>
2025-10-26 21:27:47 +0900

    feat: BaseEntity 추가

--- src/main/java/promptstudio/promptstudio/PromptstudioApplication.java
+++ src/main/java/promptstudio/promptstudio/PromptstudioApplication.java
@@ -2,8 +2,10 @@ package promptstudio.promptstudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PromptstudioApplication {
 
 	public static void main(String[] args) {
