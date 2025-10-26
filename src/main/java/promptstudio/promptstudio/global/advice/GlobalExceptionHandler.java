8138e157721b2daa96d2ca442ab5e8d718dd7718
dev-cyan <zyos1@naver.com>
2025-10-26 21:14:33 +0900

    feat: GlobalExceptionHandler 로그 추가

--- src/main/java/promptstudio/promptstudio/global/advice/GlobalExceptionHandler.java
+++ src/main/java/promptstudio/promptstudio/global/advice/GlobalExceptionHandler.java
@@ -1,15 +1,18 @@
 package promptstudio.promptstudio.global.advice;
 
+import lombok.extern.slf4j.Slf4j;
 import promptstudio.promptstudio.global.exception.http.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ProblemDetail;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
     public ProblemDetail handleBadRequestException(BadRequestException ex) {
+        log.warn("BadRequestException occur: {}", ex.getMessage(), ex);
         ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
         problemDetail.setTitle("400_BAD_REQUEST");
         problemDetail.setDetail(ex.getMessage());
@@ -18,6 +21,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UnauthorizedException.class)
     public ProblemDetail handleUnauthorizedException(UnauthorizedException ex) {
+        log.warn("UnauthorizedException occur: {}", ex.getMessage(), ex);
         ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.UNAUTHORIZED);
         problemDetail.setTitle("401_UNAUTHORIZED");
         problemDetail.setDetail(ex.getMessage());
@@ -26,6 +30,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ForbiddenException.class)
     public ProblemDetail handleForbiddenException(ForbiddenException ex) {
+        log.warn("ForbiddenException occur: {}", ex.getMessage(), ex);
         ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.FORBIDDEN);
         problemDetail.setTitle("403_FORBIDDEN");
         problemDetail.setDetail(ex.getMessage());
@@ -34,6 +39,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     public ProblemDetail handleNotFoundException(NotFoundException ex) {
+        log.warn("NotFoundException occur: {}", ex.getMessage(), ex);
         ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
         problemDetail.setTitle("404_NOT_FOUND");
         problemDetail.setDetail(ex.getMessage());
@@ -42,6 +48,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(ConflictException.class)
     public ProblemDetail handleConflictException(ConflictException ex) {
+        log.warn("ConflictException occur: {}", ex.getMessage(), ex);
         ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.CONFLICT);
         problemDetail.setTitle("409_CONFLICT");
         problemDetail.setDetail(ex.getMessage());
