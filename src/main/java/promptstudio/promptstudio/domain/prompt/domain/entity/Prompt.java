80981a6f864ea3cef6ec986f88d9474356dcc4c1
dev-cyan <zyos1@naver.com>
2025-10-28 17:20:44 +0900

    feat: Prompt 엔티티 추가

---
+++ src/main/java/promptstudio/promptstudio/domain/prompt/domain/entity/Prompt.java
@@ -0,0 +1,81 @@
+package promptstudio.promptstudio.domain.prompt.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+import promptstudio.promptstudio.domain.member.domain.entity.Member;
+import promptstudio.promptstudio.global.common.entity.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Prompt extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @Column(nullable=false)
+    private String title;
+
+    @Column(nullable=false)
+    private String introduction;
+
+    @Column(columnDefinition="TEXT")
+    private String content;
+
+    @Column(nullable=false)
+    private boolean visible;
+
+    @Column(length = 2048)
+    private String imageUrl;
+
+    @Column(columnDefinition="TEXT")
+    private String result;
+
+    @Column(nullable=false)
+    private String category;
+
+    @Column(nullable=false)
+    private boolean imageRequired;
+
+    @Column(nullable=false)
+    private String AiEnvironment;
+
+    @Column(nullable=false)
+    private Integer copyCount;
+
+    @Column(nullable=false)
+    private Integer viewCount;
+
+    @Builder
+    public Prompt(Member member, String title,
+                  String introduction, String content,
+                  boolean visible, String imageUrl,
+                  String result, String category,
+                  boolean imageRequired, String AiEnvironment,
+                  Integer copyCount, Integer viewCount) {
+        this.member = member;
+        this.title = title;
+        this.introduction = introduction;
+        this.content = content;
+        this.visible = visible;
+        this.imageUrl = imageUrl;
+        this.result = result;
+        this.category = category;
+        this.imageRequired = imageRequired;
+        this.AiEnvironment = AiEnvironment;
+        this.copyCount = copyCount;
+        this.viewCount = viewCount;
+    }
+
+}
