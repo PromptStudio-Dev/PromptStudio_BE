3c6a53715c5b00a62412677e55c1718aa02f0a55
dev-cyan <zyos1@naver.com>
2025-10-30 14:23:40 +0900

    feat: PromptController 추가

--- src/main/java/promptstudio/promptstudio/domain/prompt/application/PromptService.java
+++ src/main/java/promptstudio/promptstudio/domain/prompt/application/PromptService.java
@@ -1,8 +1,9 @@
 package promptstudio.promptstudio.domain.prompt.application;
 
+import org.springframework.web.multipart.MultipartFile;
 import promptstudio.promptstudio.domain.member.domain.entity.Member;
 import promptstudio.promptstudio.domain.prompt.dto.PromptCreateRequest;
 
 public interface PromptService {
-    void createPrompt(Member member, PromptCreateRequest request);
+    Long createPrompt(Long memberId, PromptCreateRequest request, MultipartFile file);
 }
