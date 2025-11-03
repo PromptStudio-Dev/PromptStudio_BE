4ae821e1b099248a0293df8034ea1eb3785694aa
1jae2jae3jae <drawje01@gmail.com>
2025-11-04 01:52:23 +0900

    feat: MakerRepository fetch join 쿼리 추가

--- src/main/java/promptstudio/promptstudio/domain/maker/domain/repository/MakerRepository.java
+++ src/main/java/promptstudio/promptstudio/domain/maker/domain/repository/MakerRepository.java
@@ -1,7 +1,14 @@
 package promptstudio.promptstudio.domain.maker.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import promptstudio.promptstudio.domain.maker.domain.entity.Maker;
 
+import java.util.Optional;
+
 public interface MakerRepository extends JpaRepository<Maker, Long> {
+
+    @Query("SELECT m FROM Maker m LEFT JOIN FETCH m.images WHERE m.id = :makerId")
+    Optional<Maker> findByIdWithImages(@Param("makerId") Long makerId);
 }
