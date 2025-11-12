13527e833658003ffb1c86f7825151b2e685f437
1jae2jae3jae <drawje01@gmail.com>
2025-11-12 21:09:17 +0900

    History Repository 개발

---
+++ src/main/java/promptstudio/promptstudio/domain/history/domain/repository/HistoryRepository.java
@@ -0,0 +1,23 @@
+package promptstudio.promptstudio.domain.history.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import promptstudio.promptstudio.domain.history.domain.entity.History;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface HistoryRepository extends JpaRepository<History, Long> {
+
+    @Query("SELECT h FROM History h " +
+            "LEFT JOIN FETCH h.snapshotImages " +
+            "WHERE h.maker.id = :makerId " +
+            "ORDER BY h.createdAt DESC")
+    List<History> findAllByMakerIdWithImages(@Param("makerId") Long makerId);
+
+    @Query("SELECT h FROM History h " +
+            "LEFT JOIN FETCH h.snapshotImages " +
+            "WHERE h.id = :historyId")
+    Optional<History> findByIdWithImages(@Param("historyId") Long historyId);
+}
